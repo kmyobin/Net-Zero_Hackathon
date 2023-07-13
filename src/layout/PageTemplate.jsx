257a7f9c219be5217b877679cdac49dea99a3bda
taehun0933 <taehun0933@naver.com>
2023-07-13 21:08:57 +0900

    반응형크기 개선

--- src/layout/PageTemplate.jsx
+++ src/layout/PageTemplate.jsx
@@ -1,19 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import Header from "./Header";
 
 function PageTemplate({ content }) {
-  const [contentHeight, setCt] = useState();
+  const [contentHeight, setContentHeight] = useState();
+
+  const setContentWidth = useCallback(() => {
+    const CH = document.querySelector("#contentHeight");
+    CH.style.width = contentHeight;
+  }, [contentHeight]);
+
   useEffect(() => {
-    setCt(document.querySelector("#contentHeight"));
+    setContentHeight(
+      (document.querySelector("#contentHeight").offsetHeight * 6) / 11 + "px"
+    );
   }, []);
+
+  useEffect(() => {
+    setContentWidth();
+  }, [contentHeight, setContentWidth]);
+
+  window.addEventListener("resize", () => {
+    setContentHeight(
+      (document.querySelector("#contentHeight").offsetHeight * 6) / 11 + "px"
+    );
+  });
+
   return (
     <div className="w-full h-screen flex justify-center flex-wrap items-center">
       <div
         className="relative flex h-[97.5%] w-full flex-col justify-between overflow-hidden bg-[#ECEFF4] rounded-2xl"
         id="contentHeight"
-        style={{
-          width: contentHeight && contentHeight.offsetHeight * 0.6 + "px",
-        }}
       >
         <Header />
         <div className="h-screen flex justify-center items-center">
