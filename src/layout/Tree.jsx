e689cfc4b7fd5f1c3b09bbe7302dc028c9ae315a
kmyobin <64712993+kmyobin@users.noreply.github.com>
2023-07-14 23:00:51 +0900

    kmyobin commit

--- src/layout/Tree.jsx
+++ src/layout/Tree.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import TreeIcon from "../assets//images/baricon.png";
 
-function Tree() {
+function Tree({score}) {
   return (
     <div className=" rounded-3xl bg-white text-[#929292] flex justify-between w-full">
       <div className="pl-5 pr-7 justify-center flex items-center text-sm">
-        4582
+        {score}
       </div>
       <div className="pr-2 pt-2">
         <img src={TreeIcon} alt="TreeIcon" />
