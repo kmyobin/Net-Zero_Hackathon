e9be456eb26a0923028cc61106575cf447f1dc34
kmyobin <64712993+kmyobin@users.noreply.github.com>
2023-07-14 14:44:27 +0900

    kmyobin commit

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,13 +1,29 @@
 import React from "react";
-import Logo from "../assets/images/icons/gardenerLogo.svg";
+import Tree from "./Tree";
+import Home from "../assets/images/icons/Home.svg"
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+
+  const onClickHome = () => {
+    navigate("/")
+  }
+
+  const Lo = window.location.pathname;
+
   return (
-    <nav className="flex h-20 w-full items-center pl-5 bg-slate-500">
-      <img src={Logo} width={40} height={40} alt="로고 이름" />
-      <span className="flex items-center justify-center text-xl font-bold text-[#00396E]">
-        로고 이름
-      </span>
+    <nav className="flex h-20 w-full items-center p-10 bg-[rgba(100,100,100,0)]">
+      <div className="flex justify-between w-full ">
+        <div>
+          {Lo.includes('gallery') ? (
+            <img src={Home} alt="Home" width={30} height={30} onClick={onClickHome} className="cursor-pointer" />
+          ): (
+            <div>&nbsp;</div>
+          )}
+          </div>
+        <div><Tree /></div>
+      </div>
     </nav>
   );
 }
