f4606efa38b4ec024ce7cbb35892943abe5ac4e5
kmyobin <64712993+kmyobin@users.noreply.github.com>
2023-07-14 21:12:19 +0900

    kmyobin commit

--- src/components/main/NoticeModal.jsx
+++ src/components/main/NoticeModal.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Cancel from "../../assets/images/icons/cancel.svg";
 import LevelUp from "../../assets/images/icons/levelup.svg"
+import LevelUpTree from "../../assets/images/leveluptree.png"
+import TreeIcon from "../../assets/images/notice_tree.png"
+
 function NoticeModal({ noticeOpen, setNoticeOpen }) {
   const onClickCancel = () => {
     setNoticeOpen(!noticeOpen);
   };
   return (
     <div className="absolute top-0 left-0 rounded-2xl w-full h-full z-10 bg-[rgba(0,0,0,0.4)]  flex justify-center items-center">
-      <div className="relative w-[80%] h-[70%] rounded-xl bg-white z-20 ">
+      <div className="relative w-[80%] h-[80%] rounded-xl bg-white z-20 ">
         <img
           src={Cancel}
           alt="cancel"
@@ -15,10 +18,32 @@ function NoticeModal({ noticeOpen, setNoticeOpen }) {
           onClick={onClickCancel}
         />
 
-        <div className='flex justify-center items-center'>
-          <div className='font-[1000] text-4xl mt-16'>축하합니다!</div>          
+        <div className="">
+          <div className="font-[1000] text-4xl my-[15%] flex justify-center items-center">
+            축하합니다!
+          </div>
+          <div className="flex justify-center items-center relative">
+            <img
+              src={LevelUp}
+              alt="levelup"
+              className="flex justify-center items-center w-[70%]"
+            />
+            <img
+              src={LevelUpTree}
+              alt="levelupTree"
+              className="absolute bottom-[20%] w-[30%]"
+            />
+          </div>
+          <div className="my-[8%] flex justify-center items-center text-center">
+            인덕이 님이 정성껏 키워주신 나무가 <br />두 그루의 나무로 자라났어요
+          </div>
+          <div className="flex justify-center items-center">
+            <div className="text-center rounded-full shadow-lg p-5 w-[70px] h-[70px] border-1 text-[#929292]">
+              <img src={TreeIcon} alt="treeicon" className="" />
+              <p className="pb-1 text-sm">+1</p>
+            </div>
+          </div>
         </div>
-
       </div>
     </div>
   );
