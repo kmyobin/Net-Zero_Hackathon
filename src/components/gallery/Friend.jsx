7e2d190c2c565901d523353c04093c958bf7c127
taehun0933 <taehun0933@naver.com>
2023-07-14 15:01:39 +0900

    ㄱ

---
+++ src/components/gallery/Friend.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Friend() {
+  return <div className="w-[80%] h-24 mx-auto">칭긔</div>;
+}
--- src/components/gallery/GalleryContent.jsx
+++ src/components/gallery/GalleryContent.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Friends from "./Friends";
+import Header from "../../layout/Header";
 
 export default function GalleryContent() {
   return (
     <div className="w-full bg-[#C3E2B8]">
-      <div className="flex flex-col">
+      <div className="flex flex-col mb-6">
         {/* 헤더 + 농장 */}
+        <Header />
         <img src={require("../../assets/images/platform.png")} alt="platform" />
       </div>
       <Friends />
