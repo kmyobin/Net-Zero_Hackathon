aae83a8ea96c9e77840b451e00f9b99ead539d20
kmyobin <64712993+kmyobin@users.noreply.github.com>
2023-07-14 13:25:51 +0900

    kmyobin commit

--- src/layout/PageTemplate.jsx
+++ src/layout/PageTemplate.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback } from "react";
-// import Header from "./Header";
+import Header from "./Header";
 
 function PageTemplate({ content }) {
   const [contentHeight, setContentHeight] = useState();
@@ -32,7 +32,7 @@ function PageTemplate({ content }) {
         className="relative flex h-[97.5%] w-full flex-col justify-between overflow-hidden bg-[#ECEFF4] rounded-2xl"
         id="contentHeight"
       >
-        
+        {!Lo.includes('/login') &&<Header/>}
         <div className="h-screen flex justify-center">{content}</div>
       </div>
     </div>
---
+++ src/layout/Tree.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Tree() {
+  return (
+    <div></div>
+  )
+}
+
+export default Tree
